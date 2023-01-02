11a8256a227d7f74da246cfa44e1fc17d91863e5
38elements <mh19820223@gmail.com>
2023-01-03 01:34:52 +0900

    add test

--- scripts/utils.test.ts
+++ scripts/utils.test.ts
@@ -1,8 +1,17 @@
-import { createTitle }  from './utils.js'
+import { createTitle, createDescription }  from './utils.js'
 
 describe('createTitle', () => {
   test('output', () => {
     const title = createTitle('0123456789\n012346789\n0123456789')
     expect(title).toBe('23456789')
+  })
+})
+
+describe('createDescription', () => {
+  test('output', () => {
+    const html = `foo\n${'1234"'.repeat(40)}`
+    const result = '1234&quot;'.repeat(30)
+    const description = createDescription(html)
+    expect(description).toBe(result)
   })
 })
