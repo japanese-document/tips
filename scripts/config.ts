70a404451bace028c008257852768cdb3f3c3936
38elements <mh19820223@gmail.com>
2023-01-14 15:01:13 +0900

    use promisify

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,18 +1,8 @@
-import { parseArgs } from 'node:util'
-
-const { values: { dev } } = parseArgs({
-  options: {
-    dev: {
-      type: 'boolean'
-    }
-  }
-})
-
 export const TITLE = /__TITLE__/g
 export const BODY = '__BODY__'
 export const CSS = '__CSS__'
-export const CSS_PATH = dev ? '/app.css?v=001' : '/tips/app.css?v=001'
-export const BASE_URL = dev ? 'http://127.0.0.1:8000' : 'https://japanese-document.github.io/tips'
+export const CSS_PATH = '/tips/app.css?v=001'
+export const BASE_URL = 'https://japanese-document.github.io/tips'
 export const URL = '__URL__'
 export const DESCRIPTION = /__DESCRIPTION__/g
 export const SEPARATOR = /---(.*)/s
