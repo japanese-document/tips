6455ec8e0752fdd662251f864d1ae213d7c0e2fe
38elements <mh19820223@gmail.com>
2023-01-15 00:43:21 +0900

    createURL

--- scripts/utils.test.ts
+++ scripts/utils.test.ts
@@ -1,15 +1,15 @@
-import { createTitle, createDescription, getMetaAndMd }  from './utils.js'
+import { createTitle, createDescription, getMetaAndMd, createURL }  from './utils.js'
 
 describe('createTitle', () => {
   test('output', () => {
-    const title = createTitle('0123456789\n012346789\n0123456789')
-    expect(title).toBe('23456789')
+    const title = createTitle('# foo\n012346789\n0123456789')
+    expect(title).toBe('foo')
   })
 })
 
 describe('createDescription', () => {
   test('output', () => {
-    const html = `foo\n${'1234"'.repeat(40)}`
+    const html = `# foo\n${'1234"'.repeat(40)}`
     const result = '1234&quot;'.repeat(30)
     const description = createDescription(html)
     expect(description).toBe(result)
@@ -18,8 +18,15 @@ describe('createDescription', () => {
 
 describe('getMetaAndMd', () => {
   test('output', () => {
-    const input = '{"foo": 123}\n---\n#Foo\nBar'
+    const input = '{"foo": 123}\n---\n# Foo\nBar'
     const metaAndMd = getMetaAndMd(input)
-    expect(metaAndMd).toEqual([{ foo: 123 }, '#Foo\nBar'])
+    expect(metaAndMd).toEqual([{ foo: 123 }, '# Foo\nBar'])
+  })
+})
+
+describe('createURL', () => {
+  test('output', () => {
+    const url = createURL('0123456789', 'name')
+    expect(url).toBe('https://japanese-document.github.io/tips/6789/name.html')
   })
 })
