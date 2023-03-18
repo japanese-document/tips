501da9e3bf943e3e41d7e1c47a44ddb1e94a0c6a
38elements <38elements@users.noreply.github.com>
2023-03-18 19:31:59 +0900

    test (#97)

--- scripts/utils.test.ts
+++ scripts/utils.test.ts
@@ -1,4 +1,4 @@
-import { createTitle, createDescription, getMetaAndMd, createURL, createHash }  from './utils.js'
+import { createTitle, createDescription, getMetaAndMd, createURL, createHash, createIndexItems }  from './utils.js'
 
 describe('createTitle', () => {
   test('output', () => {
@@ -35,5 +35,80 @@ describe('createHash', () => {
   test('output', () => {
     const hash = createHash('<a>?:&foo=%"\'@><')
     expect(hash).toBe('___foo______--_')
+  })
+})
+
+describe('createIndexItems', () => {
+  function createPageData() {
+    return [
+      {
+        headerOrder: 0,
+        order: 0
+      },
+      {
+        headerOrder: 0,
+        order: 1
+      },
+      {
+        headerOrder: 1,
+        order: 0
+      },
+    ].map( p => ({
+      meta: {
+        header: {
+          name: `name${p.headerOrder}`,
+          order: p.headerOrder
+        },
+        order: p.order
+      },
+      title: `title${p.order}`,
+      url: `https://example.com/${p.order}`
+    }))
+  }
+
+  let spy: jest.SpyInstance
+
+  beforeAll(() => {
+    spy = jest.spyOn(console, 'log')
+  })
+
+  afterAll(() => {
+    jest.restoreAllMocks()
+  })
+
+  test('output', () => {
+    const pages = createPageData()
+    const indexItems = createIndexItems(pages)
+    expect(indexItems).toEqual([
+      { name: 'name0', pages: [{ title: 'title0', url: 'https://example.com/0' },{ title: 'title1', url: 'https://example.com/1' }] },
+      { name: 'name1', pages: [{ title: 'title0', url: 'https://example.com/0' }] }])
+  })
+
+  test('wrong header', () => {
+    const pages = createPageData()
+    pages[1].meta.header.name = 'foo'
+    expect(() => {
+      createIndexItems(pages)
+    }).toThrow('header already exists.')
+
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(
+      [{ name: 'name0', pages: [{ title: 'title0', url: 'https://example.com/0' }] }],
+      { meta: { header: { name: 'foo', order: 0 }, order: 1 }, title: 'title1', url: 'https://example.com/1' }
+    )
+  })
+
+  test('wrong page', () => {
+    const pages = createPageData()
+    pages[1].meta.order = 0
+    expect(() => {
+      createIndexItems(pages)
+    }).toThrow('page already exists.')
+
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(
+      { title: 'title0', url: 'https://example.com/0' },
+      { meta: { header: { name: 'name0', order: 0 }, order: 0 }, title: 'title1', url: 'https://example.com/1' }
+    )
   })
 })
