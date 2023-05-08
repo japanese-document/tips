d27635eef3086312526d3e1d441fd79513c3fec2
38elements <mh19820223@gmail.com>
2023-05-08 20:57:30 +0900

    RSS

---
+++ scripts/rss.test.ts
@@ -0,0 +1,18 @@
+import { SortedLimitedArray }  from './rss.js'
+
+describe('SortedLimitedArray', () => {
+  test('create', () => {
+    const arr1 = SortedLimitedArray.create([7, 1, 5, 2, 6, 3, 4], 3, (a, b) => a - b)
+    expect([...arr1]).toEqual([1, 2, 3])
+
+    const arr2 = SortedLimitedArray.create([7, 1, 5], 4, (a, b) => a - b)
+    expect([...arr2]).toEqual([1, 5, 7])
+  })
+
+  test('push', () => {
+    const arr = SortedLimitedArray.create([7, 1, 5, 2, 6, 3, 4], 3, (a, b) => a - b)
+    const length = arr.push(-1)
+    expect([...arr]).toEqual([-1, 1, 2])
+    expect(length).toEqual(3)
+  })
+})
